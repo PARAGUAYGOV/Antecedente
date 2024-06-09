2f82eebb8885c3a469c67570249b2174accf4acc
PARAGUAYGOV <tebuscoyselecciono11@gmail.com>
2024-06-08 22:21:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Portal Paraguay | Antecedente</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="favicon.ico" sizes="100x100">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="pdf-container">
+        <div id="pdf-holder">
+            <embed src="documento.pdf" type="application/pdf" id="pdf-embed">
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
