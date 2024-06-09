6695988737ef020e7d0d84d1b8202b78ab8774ad
PARAGUAYGOV <tebuscoyselecciono11@gmail.com>
2024-06-08 22:19:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var pdfEmbed = document.getElementById("pdf-embed");
 
-    // Intenta cargar el PDF y detecta errores
-    pdfEmbed.addEventListener('error', function() {
-        // Redirige a la descarga del PDF
-        window.location.href = "documento.pdf";
-    });
-
-    // Verifica si el PDF se ha cargado correctamente
-    setTimeout(function() {
-        var isPdfLoaded = pdfEmbed.contentDocument && pdfEmbed.contentDocument.body.childNodes.length > 0;
-        if (!isPdfLoaded) {
-            pdfEmbed.dispatchEvent(new Event('error'));
-        }
-    }, 1000);
-});
